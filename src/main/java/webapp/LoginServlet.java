f8ca73845fc34faaa4ff654c977ae0297701a75d
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2024-03-12 19:18:27 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Techlanders Solutions</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1> Hi guys , This is a devops session Raman Khanna  !! </h1>");
+		out.println("<h1> Hi guys , This is a github actions session from Raman Khanna  !! </h1>");
 		out.println("</body>");
 		out.println("</html>");
 
